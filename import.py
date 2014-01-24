a6340ec497eb0119b56c4ec7a9eb885bcf563489
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2014-01-24 11:23:50 +0530

    removed dependency on quodlibet_cli

--- import.py
+++ import.py
@@ -1,16 +1,23 @@
-import sys, os, glob, qllib, shutil
+import sys, os, glob,  shutil
+import quodlibet
+from quodlibet.formats import MusicFile
 
 def import_files(ipod_control_music_dir, dest_dir):
-    print "Processing... "
-    for fname in glob.glob(os.path.join(ipod_control_music_dir,'*','*')):
+    quodlibet.config.init()
+    print "Importing music from  '%s'" % ipod_control_music_dir
+    
+    for fname in glob.glob(os.path.join(ipod_control_music_dir,'*')):
+            print "importing %s" % fname
             import_file(fname, dest_dir)
     
 def safe_read_tag(tags,key):
-    return tags[key][0].strip().replace('/',' ') if tags[key] and len(tags[key])>0 and len(tags[key][0].strip())>0 else 'Unknown ' + key.title()
+    return tags[key].strip().replace('/',' ').encode('utf-8') if tags and key in tags and len(tags[key].strip())>0 else 'Unknown ' + key.title()
 
 def import_file(ipod_file,dest_dir):
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
     try:
-        tags = qllib.AudioFile(ipod_file)
+        tags = MusicFile(ipod_file)
         artist = safe_read_tag(tags,'artist')
         album = safe_read_tag(tags,'album')
         title = safe_read_tag(tags,'title')
